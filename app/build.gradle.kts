c775cd298318ecd6b4b2f497a1815ecb55086505
Samuel Aubyn <aubynsamuel05@gmail.com>
2026-03-27 11:47:54 +0000

    update gradle and kotlin versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,4 @@
-import com.android.build.gradle.ProguardFiles.getDefaultProguardFile
-import org.gradle.kotlin.dsl.android
-import org.gradle.kotlin.dsl.kotlin
-import org.gradle.kotlin.dsl.ksp
-import org.gradle.kotlin.dsl.libs
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.internal.types.error.ErrorModuleDescriptor.platform
 
 plugins {
     alias(libs.plugins.android.application)
